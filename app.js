562c35c7c2297dcf73328cb3c2f7138a21693562
joaoUlissses <joao.ulisses.serrao@gmail.com>
2024-06-13 19:58:28 -0300

    mudanca de 10 para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,12 +11,12 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
 
-function verificarChute() {s
+function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
